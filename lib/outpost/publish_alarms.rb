3d2a612483f7f2e7af03b61e7c1f5cd4f2a55d86
Bryan Ricker <bricker88@gmail.com>
2013-04-15 16:10:41 -0700

    Git init

---
+++ lib/outpost/publish_alarms.rb
@@ -0,0 +1,7 @@
+require "outpost/publish_alarms/version"
+
+module Outpost
+  module PublishAlarms
+    # Your code goes here...
+  end
+end
---
+++ lib/outpost/publish_alarms/version.rb
@@ -0,0 +1,5 @@
+module Outpost
+  module PublishAlarms
+    VERSION = "0.0.1"
+  end
+end
